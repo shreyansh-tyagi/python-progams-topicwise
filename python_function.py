ccae991b7a2df21060f5e2be17ea8f9530bada34
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-03-26 01:30:37 +0530

    python_function has been added

--- python_function.py
+++ python_function.py
@@ -1,4 +1,13 @@
 def func1():
     print('i am inside the function')
+def func2(num):
+    if(num>=0):
+        print(f'{num} is greater than zero')
+    else:
+        print(f'{num} is less than zero')
+fun3=func2       
 func_var=func1
 print(func_var())
+num=int(input('enter the number: '))
+print(fun3(num))           
+   
