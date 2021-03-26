8435322af5a557e81fc765d0ad09788347f7598b
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-03-26 22:03:21 +0530

    passing reference to the function

--- passing reference.py
+++ passing reference.py
@@ -1,5 +1,6 @@
 def fun1():
     print('called function')
 def fun2(f):
-    f() 
-fun2(fun1)    
+    return f() #returning function
+fun2(fun1)  #passing reference of the function 
+# passing one function to another function
