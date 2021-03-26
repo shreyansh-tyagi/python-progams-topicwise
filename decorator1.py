60934736777c438be19db412db599f8a888a4841
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-03-27 02:14:41 +0530

    passing reference to the function

---
+++ decorator1.py
@@ -0,0 +1,22 @@
+def f1(func):
+    def wrapper(*args,**kwargs):
+        print('started')
+        a=func(*args,**kwargs)
+        print('ended')
+        return a
+    return wrapper
+@f1
+def f(a,b):
+    print(a+b)
+a,b=map(int,input('enter the value: ').split())
+f(a,b)
+
+@f1
+def f2(a,b):
+    if(a+b)%2==0:
+        print(f'{a}+{b} is {a+b} even')
+    else:
+        print(f'{a}+{b} is odd')    
+f2(a,b)    
+    
+    
