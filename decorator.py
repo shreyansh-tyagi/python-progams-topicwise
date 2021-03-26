4da74a41f5a07ee80825c2c63399d092bf5a80c0
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-03-26 22:45:00 +0530

    passing reference to the function

--- decorator.py
+++ decorator.py
@@ -4,7 +4,10 @@ def fun(func): #reference of f function will come to func variable
         func() #calling the f function because func contain the reference of f function therefore the control will move to the reference of f function and execute that block first
         print('ended') #control come back to this statement and start executing the from here
     return wrapper   #returning the wrapper function and calling the wrapper function 
+@fun #decorator
 def f():
     print('hello')
-x=fun(f)  #calling function fun and passing the reference of f function    
-x() #calling function 
+f()
+#x=fun(f) #this line work as the decorator #calling function fun and passing the reference of f function    
+#x() #calling function 
+#instead of doing this we can use the way how to actually declare the decorator
