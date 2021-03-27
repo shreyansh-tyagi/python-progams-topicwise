82bf3ed583c8adff6535e880d1fe5ee9ccad3b91
shreyansh-tyagi <sunnytyagi886@gmail.com>
2021-03-27 18:53:35 +0530

    sep and end parameter in python

---
+++ sep and end parameter.py
@@ -0,0 +1,7 @@
+print('hello, i am shreyansh tyagi from ghazaibad\nmy email address is shreytyagi3221@gmail.com\nmy nickname is sunny tyagi\nstudying from geeksforgeeks\n\n')
+print('hello','i am shreyansh tyagi from ghaziabad',sep=',',end='\n')
+print('my email address is shreytyagi3221','gmail.com',sep='@',end='\n')
+print('my nickname is sunny tyagi',sep='',end='\n')
+print('i am studying from','geeks',sep=' ',end='')
+print('forgeeks',sep='#',end='\n')
+print
